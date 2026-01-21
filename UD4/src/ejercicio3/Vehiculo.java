c3ccc3da6ca289831ef6af7a65deb37693b644ac
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-01-21 09:49:09 +0100

    Vehiculo.java

--- UD4/src/ejercicio3/Vehiculo.java
+++ UD4/src/ejercicio3/Vehiculo.java
@@ -20,9 +20,31 @@ public class Vehiculo {
 	}
 	
 	// Metodos
-	
+	public void mostrarInformacion() {
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Marca: " + marca);
+        System.out.println("Año de fabricación: " + anioFabricacion);
+        System.out.println("Precio: " + precio + " €");
+        System.out.println("Tipo de vehículo: " + tipo);
+    }
+
+	public boolean esAntiguo() {
+        int anioActual = java.time.Year.now().getValue();
+        return (anioActual - anioFabricacion) > 10;
+    }
+
+	public void aplicarDescuento(double porcentaje) {
+        if (porcentaje > 0 && porcentaje <= 100) {
+            double descuento = precio * (porcentaje / 100);
+            precio -= descuento;
+            System.out.println("Descuento aplicado: " + porcentaje + "%. Nuevo precio: " + precio + " €");
+        } else {
+            System.out.println("El porcentaje de descuento debe estar entre 0 y 100.");
+        }
+    }
 	
 	
 	
 
 }
+
