aef7c1e6fd7b6c9aa43b2535841f0f9ffc67a1f1
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-03-17 12:15:52 +0100

    Main.java

--- UD5/src/ejercicio6/Main.java
+++ UD5/src/ejercicio6/Main.java
@@ -2,9 +2,15 @@ package ejercicio6;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        double a = 10;
+        double b = 5;
 
+        System.out.println("Suma: " + Calculadora.suma(a, b));
+        System.out.println("Resta: " + Calculadora.resta(a, b));
+        System.out.println("Multiplicación: " + Calculadora.multiplicacion(a, b));
+        System.out.println("División: " + Calculadora.division(a, b));
+    }
 }
+ 
