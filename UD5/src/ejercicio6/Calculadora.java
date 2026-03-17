cd2a2d09f3000d59afe235b81497d9e0880fd2b0
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-03-17 12:14:43 +0100

    Calculadora.java

--- UD5/src/ejercicio6/Calculadora.java
+++ UD5/src/ejercicio6/Calculadora.java
@@ -2,12 +2,24 @@ package ejercicio6;
 
 public class Calculadora {
 
-	static double suma(double a, double b) {
-		return a + b;
-	}
-	
-	
-	
-	
+    static double suma(double a, double b) {
+        return a + b;
+    }
+
+    static double resta(double a, double b) {
+        return a - b;
+    }
+
+    static double multiplicacion(double a, double b) {
+        return a * b;
+    }
+
+    static double division(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: no se puede dividir entre 0");
+            return 0;
+        }
+        return a / b;
+    }
 
 }
