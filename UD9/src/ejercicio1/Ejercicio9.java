a7f93b875533afc11566ee54d7a3a73e71e93d26
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-05-12 13:23:53 +0200

    Add files via upload

---
+++ UD9/src/ejercicio1/Ejercicio9.java
@@ -0,0 +1,40 @@
+package ejercicio1;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+
+        File archivo = new File("notas.txt");
+
+        try {
+
+            if (archivo.exists()) {
+
+                System.out.println("El archivo existe.");
+                System.out.println("Nombre: " + archivo.getName());
+                System.out.println("Ruta absoluta: " + archivo.getAbsolutePath());
+
+            } else {
+
+                boolean creado = archivo.createNewFile();
+
+                if (creado) {
+                    System.out.println("El archivo no existía y fue creado correctamente.");
+                    System.out.println("Nombre: " + archivo.getName());
+                    System.out.println("Ruta absoluta: " + archivo.getAbsolutePath());
+                } else {
+                    System.out.println("No se pudo crear el archivo.");
+                }
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Ocurrió un error de entrada/salida.");
+            e.printStackTrace();
+
+        }
+    }
+}
