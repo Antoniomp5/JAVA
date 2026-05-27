e332ee4142c27be8ce868ff8de9a589b39f52cc0
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-05-27 10:56:39 +0200

    Add files via upload

---
+++ UD10/src/ejercicios/Artistas.java
@@ -0,0 +1,51 @@
+package ejercicios;
+
+import java.io.Serializable;
+
+public class Artistas implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	private String nacionalidad;
+	private int annosExperiencia;
+	
+	public Artistas(String nombre, String nacionalidad, int annosExperiencia) {
+		super();
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+		this.annosExperiencia = annosExperiencia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public int getAnnosExperiencia() {
+		return annosExperiencia;
+	}
+
+	public void setAnnosExperiencia(int annosExperiencia) {
+		this.annosExperiencia = annosExperiencia;
+	}
+	
+	
+
+	
+
+}
+
